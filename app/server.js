b0d737ad575f1ff0aa04c8ade92a61a37b85ff74
mhama02 <mayuri.mhatre14@gmail.com>
2019-10-31 11:17:31 -0700

    78. 611, 502 added

--- app/server.js
+++ app/server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var serialize = require('node-serialize');
+var cprocess = require('child_process');
+var libxmljs = require('libxmljs');
 var app = express();
 
 var converter = require("./converter");
@@ -16,13 +19,17 @@ process.on('SIGTERM', gracefulShutdown);
 // listen for INT signal (e.g. Ctrl+C).
 process.on('SIGINT', gracefulShutdown);
 
-app.get("/rgbToHex", function(req, res) {
-  // CWE-95: PASS
+// Id:          CWE-95
+// Description: Eval Injection
+// Exploit URL: http://localhost:3000/cwe95/rgbToHex?red=255&green=255&blue=255
+// Status:      PASS
+app.get("/cwe95/rgbToHex", function(req, res) {
   // To fix these security vulnerabilities, 
   // Replace the three eval() statements with their parseInt() versions.
   var red = eval(req.query.red);
   var green = eval(req.query.green, 10);
   var blue  = eval(req.query.blue, 10);
+
   // var red   = parseInt(req.query.red, 10);
   // var green = parseInt(req.query.green, 10);
   // var blue  = parseInt(req.query.blue, 10);
@@ -38,47 +45,77 @@ app.get("/hexToRgb", function(req, res) {
 
 // Id:          CWE-73
 // Description: External Control of File Name or Path
-// Exploit URL: http://localhost:3000/download?file=README.md
+// Exploit URL: http://localhost:3000/cwe73/download?file=README.md
 // Status:      PASS
-app.get('/download', function (req, res) {
+app.get('/cwe73/download', function (req, res) {
   res.download(req.query.file);
 });
 
 // Id:          CWE-79
 // Description: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')
-// Exploit URL: http://localhost:3000/echo?text=hello
+// Exploit URL: http://localhost:3000/cwe79/echo?text=hello
 // Status:      PASS
-app.get('/echo', function (req, res) {
+app.get('/cwe79/echo', function (req, res) {
   res.send("<p>You sent this: " + req.query.text + "</p>")
 });
 
 // Id:          CWE-113
 // Description: Improper Neutralization of CRLF Sequences in HTTP Headers ('HTTP Response Splitting')
-// Exploit URL: http://localhost:3000/split?key=myKey&value=myValueThatCouldHaveCRLFs
+// Exploit URL: http://localhost:3000/cwe113/split?key=myKey&value=myValueThatCouldHaveCRLFs
 // Status:      PASS
-app.get('/split', function (req, res) {
+app.get('/cwe113/split', function (req, res) {
   res.append(req.query.key, req.query.value);
   res.status(200).send('Check your headers!');
 });
 
 // Id:          CWE-201
 // Description: Information Exposure Through Sent Data
-// Exploit URL: http://localhost:3000/exposure?text=sensitive
+// Exploit URL: http://localhost:3000/cwe201/exposure?text=sensitive
 // Status:      PASS
-app.get('/exposure', function (req, res) {
+app.get('/cwe201/exposure', function (req, res) {
   res.send(req.query.text);
 });
 
 // Id:          CWE-601
 // Description: URL Redirection to Untrusted Site ('Open Redirect')
-// Exploit URL: http://localhost:3000/redirect?text=www.maliciouswebsite.com
+// Exploit URL: http://localhost:3000/cwe601/redirect?text=www.maliciouswebsite.com
 // Status:      PASS
-app.get('/redirect', function (req, res) {
+app.get('/cwe601/redirect', function (req, res) {
   res.redirect("http://localhost:3000/echo?text=" + req.query.text + " (Redirected)");
+});
+
+// Id:          CWE-502
+// Description: Deserialization of Untrusted Data
+// Exploit URL: http://localhost:3000/cwe502/serialize?foo={"rce":"_$$ND_FUNC$$_function (){console.log('exploited')}()"}
+// Status:      PASS
+app.get("/cwe502/serialize", function(req, res) {
+  serialize.unserialize(req.query.foo);
+  res.send("node-serialize");
+});
+
+// Id:          CWE-78
+// Description: OS Command Injection
+// Exploit URL: http://localhost:3000/cwe78/childprocess?foo=pwd
+// Status:      PASS
+app.get("/cwe78/childprocess", function(req, res) {
+  cprocess.exec(req.query.foo, (error,stdout,stderr) => {
+  if (error) {
+    console.log("Taking error-path through testcase (not necessarily a problem).");
+  }});
+  res.send("child_process");
+});
+
+// Id:          CWE-611
+// Description: Improper Restriction of XML External Entity Reference
+// Exploit URL: http://localhost:3000/cwe611/xmlref/?xml=<xml>xml</xml>
+// Status:      PASS
+app.get('/cwe611/xmlref', function (req, res) {
+  var xmlout = libxmljs.parseXmlString(req.query.xml, {noent:true});
+	res.send(xmlout.childNodes()[0].toString());
 });
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
-  console.log('node-api-goat app listening at port %s', port);
+  //console.log('node-api-goat app listening at port %s', port);
 });
 module.exports = server;
--- test/server.js
+++ test/server.js
@@ -14,17 +14,14 @@ describe("node-api-goat API test", function () {
     server.close();
   });
 
-  describe('Color Code Converter API', function () {
-    this.timeout(25000);
-    describe("CWE-73: External Control of File Name or Path", function() {
-      it('downloads a sensitive file from the server', function (done) {
-          request(server)
-            .get('/download?file=package.json')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
-            done();
-      });
+  describe("CWE-73: External Control of File Name or Path", function() {
+    it('downloads a sensitive file from the server', function (done) {
+        request(server)
+          .get('/cwe73/download?file=package.json')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200);
+          done();
     });
   });
 
@@ -32,7 +29,7 @@ describe("node-api-goat API test", function () {
     this.timeout(25000);
     it('echoes back what you send in the text query string parameter', function (done) {
       request(server)
-        .get('/echo?text=hello')
+        .get('/cwe79/echo?text=hello')
         .expect('Content-Type', /text/)
         .expect(200)
         .end((err, res) => {
@@ -45,7 +42,7 @@ describe("node-api-goat API test", function () {
   describe("CWE-113: Improper Neutralization of CRLF Sequences in HTTP Headers ('HTTP Response Splitting')", function () {
     it('appends a file name to the HTTP response', function (done) {
         request(server)
-          .get('/split?key=myKey&value=myValueThatCouldHaveCRLFs')
+          .get('/cwe113/split?key=myKey&value=myValueThatCouldHaveCRLFs')
           .expect(200);
           done();
     });
@@ -54,7 +51,7 @@ describe("node-api-goat API test", function () {
   describe("CWE-201: Information Exposure Through Sent Data", function () {
     it('echoes back what you send in the text query string parameter via a redirect', function (done) {
         request(server)
-          .get('/exposure?text=sensitive')
+          .get('/cwe201/exposure?text=sensitive')
           .expect(200)
           .end((err, res) => {
             res.text.should.be.include('sensitive');
@@ -66,16 +63,16 @@ describe("node-api-goat API test", function () {
   describe("CWE-601: URL Redirection to Untrusted Site ('Open Redirect')", function () {
     it('echoes back what you send in the text query string parameter via a redirect', function (done) {
         request(server)
-          .get('/redirect?text=hello')
+          .get('/cwe601/redirect?text=hello')
           .expect(302); // HTTP response code for Redirect is 302
           done();
     });
   });
 
-  describe("RGB to Hex conversion", function () {
-    it('returns the color in hex', function (done) {
+  describe("CWE-95: Eval Injection", function () {
+    it('RGB to Hex conversion', function (done) {
       request(server)
-        .get('/rgbToHex?red=255&green=255&blue=255')
+        .get('/cwe95/rgbToHex?red=255&green=255&blue=255')
         .expect(200)
         .end((err, res) => {
           res.text.should.be.include('ffffff');
@@ -95,4 +92,41 @@ describe("node-api-goat API test", function () {
         });
     });
   });
+
+  describe("CWE-502: Deserialization of Untrusted Data", function () {
+    it('Deserializes untrusted data without verification of result data', function (done) {
+      request(server)
+        .get('/cwe502/serialize?foo={"rce":"_$$ND_FUNC$$_function (){console.log(\'exploited\')}()"}')
+        .expect(200)
+        .end((err, res) => {
+          res.text.should.be.include('node-serialize');
+          return done();
+        });
+    });
+  });
+
+  describe("CWE-78: OS Command Injection", function () {
+    it('Command injection exploit', function (done) {
+      request(server)
+        .get('/cwe78/childprocess?foo=pwd')
+        .expect(200)
+        .end((err, res) => {
+          res.text.should.be.include('child_process');
+          return done();
+        });
+    });
+  });
+
+  describe("CWE-611: Improper Restriction of XML External Entity Reference", function () {
+    it('XML evaluation', function (done) {
+      request(server)
+        .get('/cwe611/xmlref/?xml=<xml>xml</xml>')
+        .expect(200)
+        .end((err, res) => {
+          res.text.should.be.include('xml');
+          return done();
+        });
+    });
+  });
+
 });
